f18cf2333cc1bc15cc1b4a9bd3f601289a4c1e32
Gavin Hawwari <122170888+gavinhawaari@users.noreply.github.com>
2024-06-13 00:18:53 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,5 +1,3 @@
-// api/server.js
-
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -13,7 +11,10 @@ app.get('/api/server', async (req, res) => {
             headers: {
                 'Referer': referer,
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                'User-Agent': 'Mozilla/5.0 (Linux; Android 11; RMX3231 Build/RP1A.201005.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.148 Mobile Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Linux; Android 11; RMX3231 Build/RP1A.201005.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.148 Mobile Safari/537.36',
+                'Cache-Control': 'no-cache',
+                'Pragma': 'no-cache',
+                'Connection': 'keep-alive'
             },
             responseType: 'stream'
         });
