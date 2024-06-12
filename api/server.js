180cb11ec8848f162c5152a60f17474b3688aba8
Gavin Hawwari <122170888+gavinhawaari@users.noreply.github.com>
2024-06-13 02:05:29 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 // Middleware untuk mengizinkan CORS (Cross-Origin Resource Sharing)
 app.use((req, res, next) => {
@@ -12,20 +12,11 @@ app.use((req, res, next) => {
 // Endpoint untuk melakukan proxy terhadap URL yang diberikan
 app.get('/api/server', async (req, res) => {
   const iframeUrl = req.query.iframe;
-
+  
   try {
-    // Konfigurasi untuk Axios
-    const axiosConfig = {
-      headers: {
-        'Referer': 'https://new8.ngefilm21.yachts/',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 11; RMX3231 Build/RP1A.201005.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.148 Mobile Safari/537.36'
-      }
-    };
-
-    // Melakukan GET request ke URL iframe yang diberikan dengan header Referer
-    const response = await axios.get(iframeUrl, axiosConfig);
-
+    // Melakukan GET request ke URL iframe yang diberikan
+    const response = await axios.get(iframeUrl);
+    
     // Mengembalikan response dari URL tersebut ke client
     res.send(response.data);
   } catch (error) {
@@ -34,7 +25,7 @@ app.get('/api/server', async (req, res) => {
   }
 });
 
-// Server listening di port yang ditentukan oleh Vercel atau 3000 jika lokal
+// Server listening di port 3000
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
