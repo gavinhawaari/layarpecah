10e522b8e09e34cdd6b89795e76b7cfde70f522a
Gavin Hawwari <122170888+gavinhawaari@users.noreply.github.com>
2024-06-13 00:33:29 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,30 +1,28 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
+const axios = require('axios');
 const app = express();
 
 app.get('/api/server', async (req, res) => {
     const videoUrl = req.query.url;
+    const referer = 'https://artist.dutamovie21.cloud/eps/open-bo-lagi-semakin-panas-semakin-ganas-s01-ep01/';
 
     try {
-        const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-        const page = await browser.newPage();
-
-        // Set user agent and other headers
-        await page.setUserAgent('Mozilla/5.0 (Linux; Android 11; RMX3231 Build/RP1A.201005.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.148 Mobile Safari/537.36');
-        await page.setExtraHTTPHeaders({
-            'Referer': 'https://artist.dutamovie21.cloud/eps/open-bo-lagi-semakin-panas-semakin-ganas-s01-ep01/',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'
+        const response = await axios.get(videoUrl, {
+            headers: {
+                'Referer': referer,
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'User-Agent': 'Mozilla/5.0 (Linux; Android 11; RMX3231 Build/RP1A.201005.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.148 Mobile Safari/537.36',
+                'Cache-Control': 'no-cache',
+                'Pragma': 'no-cache',
+                'Connection': 'keep-alive'
+            },
+            responseType: 'text', // Change responseType to 'text'
+            responseEncoding: 'utf8' // Specify responseEncoding to ensure correct character encoding
         });
 
-        await page.goto(videoUrl, { waitUntil: 'networkidle2' });
-
-        // Get the content of the page
-        const content = await page.content();
-
-        res.setHeader('Content-Type', 'text/html');
-        res.send(content);
-
-        await browser.close();
+        // Set the content type to text/html with UTF-8 encoding
+        res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+        res.send(response.data);
     } catch (error) {
         console.error(error);
         res.status(500).send('Error fetching the video!!');
