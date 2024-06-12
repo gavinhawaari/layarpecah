1a74ee5b23925b00c55edcf1be282f58014dcbb6
Gavin Hawwari <122170888+gavinhawaari@users.noreply.github.com>
2024-06-13 02:43:18 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+// File: /api/server.js
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
@@ -8,17 +9,38 @@ app.use((req, res, next) => {
   next();
 });
 
-// Konfigurasi proxy middleware
-const apiProxy = createProxyMiddleware({
-  target: 'https://vidhidepre.com',
-  changeOrigin: true, // Ubah origin request menjadi target URL
-  secure: false, // Bolehkan proxying ke target HTTPS tanpa verifikasi sertifikat
-  headers: {
-    'Referer': 'https://new8.ngefilm21.yachts/' // Atur header Referer
-  }
-});
+// Middleware untuk menangani permintaan dengan query string iframe
+app.get('/api/server', (req, res, next) => {
+  const iframeUrl = req.query.iframe;
 
-// Gunakan middleware proxy untuk semua permintaan ke /api/proxy
-app.use('/api/server', apiProxy);
+  // Pastikan query string iframe tidak kosong
+  if (!iframeUrl) {
+    return res.status(400).send('Parameter iframe harus disertakan');
+  }
+
+  // Konfigurasi proxy middleware
+  const apiProxy = createProxyMiddleware({
+    target: iframeUrl,
+    changeOrigin: true, // Ubah origin request menjadi target URL
+    secure: false, // Bolehkan proxying ke target HTTPS tanpa verifikasi sertifikat
+    headers: {
+      'Referer': 'https://new8.ngefilm21.yachts/' // Atur header Referer
+    },
+    pathRewrite: {
+      '^/': '', // Hapus bagian / dari path
+    },
+    onProxyReq: (proxyReq, req, res) => {
+      // Setel header Referer ke target URL
+      proxyReq.setHeader('Referer', iframeUrl);
+    },
+    onError: (err, req, res) => {
+      console.error('Proxy Error:', err);
+      res.status(500).send('Proxy Error');
+    }
+  });
+
+  // Gunakan middleware proxy untuk permintaan dengan query string /?iframe=
+  apiProxy(req, res, next);
+});
 
 module.exports = app;
