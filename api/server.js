b422799c1d8f88388aad4b53c4ebc1dd4b30d216
Gavin Hawwari <122170888+gavinhawaari@users.noreply.github.com>
2024-06-12 22:24:02 +0700

    Create server.js

---
+++ api/server.js
@@ -0,0 +1,30 @@
+// api/server.js
+
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.get('/server', async (req, res) => {
+    const videoUrl = req.query.url;
+    const referer = 'https://new8.ngefilm21.yachts/';
+
+    try {
+        const response = await axios.get(videoUrl, {
+            headers: {
+                'Referer': referer
+            },
+            responseType: 'stream'
+        });
+
+        res.setHeader('Content-Type', response.headers['content-type']);
+        response.data.pipe(res);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching the video');
+    }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Proxy server running at http://localhost:${port}`);
+});
