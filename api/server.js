3feb9548cffa86b0e2049afb60bc3fb621398cad
Gavin Hawwari <122170888+gavinhawaari@users.noreply.github.com>
2024-06-12 22:46:52 +0700

    Update accept and useragent server.js

--- api/server.js
+++ api/server.js
@@ -11,7 +11,9 @@ app.get('/api/server', async (req, res) => {
     try {
         const response = await axios.get(videoUrl, {
             headers: {
-                'Referer': referer
+                'Referer': referer,
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'User-Agent': 'Mozilla/5.0 (Linux; Android 11; RMX3231 Build/RP1A.201005.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.148 Mobile Safari/537.36'
             },
             responseType: 'stream'
         });
@@ -20,7 +22,7 @@ app.get('/api/server', async (req, res) => {
         response.data.pipe(res);
     } catch (error) {
         console.error(error);
-        res.status(500).send('Error fetching the video');
+        res.status(500).send('Error fetching the video!!');
     }
 });
 
